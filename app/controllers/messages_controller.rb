29f2cd63f535d6bfca075904643a7135dd8141f1
kawakamir <rui.kawakami@gmail.com>
2018-07-29 11:37:34 +0900

    create message-view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,27 @@
 class MessagesController < ApplicationController
+before_action :set_group
+
   def index
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
   end
 
   def create
-    @message = Message.create(text: message_params[:text], image: message_params[:image], user: current_user.id, group: message_params[:group])
+    @message = Message.new(message_params)
+    if @message = @group.messages.new(message_params)
+    else
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください'
+      render :index
+    end
   end
 
+  private
   def message_params
     permit.params(:text, :image)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
 end
