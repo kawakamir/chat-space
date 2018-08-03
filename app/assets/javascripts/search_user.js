8022728019fc75cfe992f2940993fc654e3688e5
kawakamir <rui.kawakami@gmail.com>
2018-08-03 19:22:58 +0900

    finish icremental-search

--- app/assets/javascripts/search_user.js
+++ app/assets/javascripts/search_user.js
@@ -27,7 +27,7 @@ function appendNoUser(user) {
       dataType: 'json'
     })
 
-    .done(function(users) {
+    .done( function(users) {
       $("#user-search-result").empty();
       if (users.length !== 0) {
         users.forEach(function(user){
@@ -41,5 +41,27 @@ function appendNoUser(user) {
     .fail(function() {
       alert('ユーザー検索に失敗しました。')
     })
+  });
+
+var group_list = $("#chat-group-users")
+
+function appendUserToGroup(userName, userId) {
+  var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${userId}'>
+                <input name='group[user_ids][]' type='hidden' value='${userId}'>
+                <p class='chat-group-user__name'>${userName}</p>
+                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+              </div>`
+  group_list.append(html)
+}
+  $("#user-search-result").on("click", ".user-search-add",function() {
+    var userName = $(this).attr("data-user-name");
+    var userId = $(this).attr("data-user-id");
+    appendUserToGroup(userName, userId);
+    $(this).parent(".chat-group-user").remove();
+  });
+
+  $("#chat-group-users").on("click", ".user-search-remove", function() {
+    console.log("hello!")
+    $(this).parent(".chat-group-user").remove();
   });
 });
