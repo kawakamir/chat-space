0a282b7d796089fa06d04672c6cfea8574c8afe6
kawakamir <rui.kawakami@gmail.com>
2018-08-04 19:33:36 +0900

    fix2 icremental-search

--- app/assets/javascripts/search_user.js
+++ app/assets/javascripts/search_user.js
@@ -1,5 +1,5 @@
 $(function() {
-
+var preInput
 var search_list = $("#user-search-result");
 
 function appendUser(user) {
@@ -19,27 +19,29 @@ function appendNoUser(user) {
 
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
-
-    $.ajax ({
-      type: 'GET',
-      url: '/users',
-      data: { keyword:input },
-      dataType: 'json'
-    })
-    .done( function(users) {
-      $("#user-search-result").empty();
-      if (users.length !== 0) {
-        users.forEach(function(user){
-          appendUser(user);
-        });
-      }
-      else {
-        appendNoUser("一致するユーザーはいません。")
-      }
-    })
-    .fail(function() {
-      alert('ユーザー検索に失敗しました。')
-    })
+    $("#user-search-result").empty();
+    if (input != preInput && input.length !== 0) {
+      $.ajax ({
+        type: 'GET',
+        url: '/users',
+        data: { keyword:input },
+        dataType: 'json'
+      })
+      .done( function(users) {
+        if (users.length !== 0) {
+          users.forEach(function(user){
+            appendUser(user);
+          });
+        }
+        else {
+          appendNoUser("一致するユーザーはいません。")
+        }
+      })
+      .fail(function() {
+        alert('ユーザー検索に失敗しました。')
+      })
+    }
+    preInput = input
   });
 
 var group_list = $("#chat-group-users")
